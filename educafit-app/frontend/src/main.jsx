23191b6c58dad4f2ddb79bfa9456b9f0c93828e4
Bruno <38926828+Bruno-JA@users.noreply.github.com>
2025-05-28 00:03:02 +0200

    cambiado nombre de contenedor principal a "contenedor-principal"

--- educafit-app/frontend/src/main.jsx
+++ educafit-app/frontend/src/main.jsx
@@ -5,6 +5,8 @@ import './main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <div className='contenedor-principal'>
+      <App />
+    </div>
+  </StrictMode>
 )
