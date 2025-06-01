ed5f26fa4022ab6bb3144186a5e6570d66f50ba4
Bruno <38926828+Bruno-JA@users.noreply.github.com>
2025-06-01 21:59:30 +0200

    Cambios pequeños para permitir la llegada correcta de los datos desde la API

--- educafit-app/frontend/src/components/tarjetaEjercicio.jsx
+++ educafit-app/frontend/src/components/tarjetaEjercicio.jsx
@@ -3,13 +3,13 @@ import "./TarjetaEjercicio.css";
 
 // Pequeño componente para mostrar una tarjeta de ejercicio con su título e imagen al seleccionar una categoría/buscar un ejercicio específico
 
-export default function TarjetaEjercicio({ titulo, imagen }) {
+export default function TarjetaEjercicio({ ejercicio, onClick }) {
   return (
-    <div className="tarjeta-ejercicio">
+    <div className="tarjeta-ejercicio" onClick={onClick}>
       <div className="miniatura">
-        <img src={imagen} alt={titulo} />
+        <img src={ejercicio.imagen} alt={ejercicio.nombre} />
       </div>
-      <div className="titulo-ejercicio">{titulo}</div>
+      <div className="titulo-ejercicio">{ejercicio.nombre}</div>
     </div>
   );
 }
